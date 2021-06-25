10566186be466315562ea4f6efc26bf2fbcfb3d3
mksvk <mksvkrishnareddy@gmail.com>
2021-06-25 13:51:17 +0530

    third commit

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ app.use(express.static(__dirname + '/public'));
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+    
     socket.on('disconnect', () => {
         console.log('user disconnected');
       });
